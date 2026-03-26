5d3d3aad603629271f8a620b6ea07fa0746e7af4
Vedika-creates <vedikak1312@gmail.com>
2026-03-26 22:10:07 +0530

    javascript rivisin day1

---
+++ BASIC/datatypes.js
@@ -0,0 +1,21 @@
+let num =10;
+console.log(num);
+
+let string ="vedika";
+console.log(string);
+
+let bool = true;
+console.log(bool);
+
+let nullVar = null;
+console.log(nullVar);
+
+let undefinedVar;
+console.log(undefinedVar);
+
+let symbol = Symbol('description');
+console.log(symbol);
+
+let bigint = 1234567890123456789012345678901234567890n;
+console.log(bigint);
+    
