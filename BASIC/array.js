ac33cf3031c116024346a991a24a44af5702e333
Vedika-creates <vedikak1312@gmail.com>
2026-03-29 23:24:11 +0530

    Your commit message

---
+++ BASIC/array.js
@@ -0,0 +1,13 @@
+//array
+let arr =[1,2,3,4,5,6,7];
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
+
+//accesing the array
+ let arr1 =[34,53,24,67,13];
+ console.log(arr1[0]);
+ console.log(arr1[1]);
+ console.log(arr1[2]);
+ console.log(arr1[3]);
+ console.log(arr1[4]);
---
+++ BASIC/operationsonarray.js
@@ -0,0 +1,20 @@
+//operations on array
+let a =[1,2,3,4,5];
+console.log(a); 
+let b =[6,7,8,9,10];
+console.log(b); 
+
+console.log(a+b);
+
+//summation on the array
+let sum1 = 0;
+for(let i=0;i<a.length;i++){
+    sum1 += a[i];
+}
+console.log(sum1);
+
+let sum2 = 0;
+for(let i=0;i<a.length;i++){
+    sum2 += a[i];
+}
+console.log(sum2);
