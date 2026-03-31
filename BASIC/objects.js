97fc6c7c3b52d7c48f259944ad2335fd4a17755b
Vedika-creates <vedikak1312@gmail.com>
2026-03-31 23:24:37 +0530

    Your commit message

---
+++ BASIC/objects.js
@@ -0,0 +1,13 @@
+//craeting simple  object
+const person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+console.log(person);
+
+const city={
+    name:"Mumbai",
+    population: 12000000
+};
+console.log(city);
---
+++ BASIC/operationsonobject.js
@@ -0,0 +1,16 @@
+// operations on objects
+const person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+console.log(person);
+ //adding in person
+
+ person.country = "India";
+ console.log(person);
+
+ //deleting in person
+
+ delete person.country;
+ console.log(person);
